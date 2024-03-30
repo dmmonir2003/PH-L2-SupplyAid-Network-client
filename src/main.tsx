87607bef24a90603e7c90a48d889dbba15692574
dmmonir2003 <dmmonir2003@gmail.com>
2024-03-30 14:42:27 +0600

    connect redux and router file in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from './redux/store'
+import router from './routes/router'
+import {
+  RouterProvider,
+} from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}>
+        </RouterProvider>
+      </PersistGate>
+
+    </Provider>
   </React.StrictMode>,
 )
