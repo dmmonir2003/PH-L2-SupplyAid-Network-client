f247a970ae19a6335bb5d4a751b266e1cf1dbdb4
dmmonir2003 <dmmonir2003@gmail.com>
2024-03-30 14:28:06 +0600

    mainLayout file connect app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-
+import MainLayout from "./layout/mainLayOut/MainLayout"
+import { useAppSelector } from "./redux/hooks";
 
 function App() {
 
 
+  const { darkMode } = useAppSelector((store) => store.theme);
   return (
-    <></>
+    <>
+      <div className={` min-h-screen w-full max-w-7xl ${darkMode ? "bg-black text-white" : ""}`}
+
+
+
+
+      ><MainLayout></MainLayout></div>
+    </>
   )
 }
 
