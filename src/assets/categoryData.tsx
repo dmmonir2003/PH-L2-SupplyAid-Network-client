943b49ad77054e9452c7788edb038abfa5de23e4
dmmonir2003 <dmmonir2003@gmail.com>
2024-03-30 14:33:13 +0600

    add logo and category data in assets folder

---
+++ src/assets/categoryData.tsx
@@ -0,0 +1,8 @@
+
+export const supplyCategoris = [
+    { id: 1, category: 'Hygiene Products' },
+    { id: 2, category: 'General Fund.' },
+    { id: 3, category: 'Orphan Fund' },
+    { id: 4, category: 'Baby Essentials' },
+    { id: 5, category: 'Food Package' }
+]
